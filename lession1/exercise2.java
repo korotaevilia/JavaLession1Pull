fb2393cb29ea42aa7455e10fd4d8730b135af51c
korotaevilia <ilia.korotaev@domru.ru>
2019-07-28 22:24:15 +0500

    для сдачи задания

---
+++ lession1/exercise2.java
@@ -0,0 +1,15 @@
+package lession1;
+
+public class exercise2 {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 10;
+        int c = 20000;
+        long d = 99999999999999999L;
+        float e = 131.2f;
+        double f = 19.2;
+        char g = 'n';
+        boolean h = true;
+
+    } ;}
---
+++ lession1/exercise4.java
@@ -0,0 +1,12 @@
+package lession1;
+
+public class exercise4 {
+    public static void main(String[] args) {
+        System.out.println(equal(10,2));
+    }
+
+        public static boolean equal(int a,int b){
+            if ((a+b)>=10 &&(a+b)<=20) return true;
+                else return false;
+                }
+}
